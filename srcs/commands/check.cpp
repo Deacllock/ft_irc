13e1c22123f51578ef897c0fbaa774d6feb675d7
Romane LESEUR <rleseur@e2r8p20.clusters.42paris.fr>
2022-12-22 00:40:08 +0100

    command check - miss check prefix

--- srcs/commands/check.cpp
+++ srcs/commands/check.cpp
@@ -8,6 +8,7 @@ bool	has_prefix(std::string msg, size_t *i)
 bool	is_good_command(std::string msg, int *i)
 {
 	if (isdigit(msg[0]))
+	{
 		if (isdigit(msg[1]) && isdigit(msg[2]) && msg[3] == ' ')
 		{
 			*i += 4;
@@ -15,8 +16,10 @@ bool	is_good_command(std::string msg, int *i)
 		}
 		else
 			return false;
+	}
+
 	int j = 0;
-	for (; msg[j] != ' '; j++)
+	for (; msg[j] != ' ' || msg[j] != '\r'; j++)
 		if (!isalpha(msg[j]))
 			return false;
 	*i += j;
@@ -24,8 +27,52 @@ bool	is_good_command(std::string msg, int *i)
 	return true;
 }
 
+bool	nospcrlfcl(char c)
+{
+	if (c == NULL || c == '\r' || c == '\n' || c == ' ' || c == ':')
+		return false;
+	return true;
+}
+
+bool	is_space_middle(std::string msg, int *i)
+{
+	int j = 0;
+	if (msg.at(j) != ' ')
+		return false;
+	j++;
+	if (!nospcrlfcl(msg.at(j)))
+		return false;
+	j++;
+	for (; 1; j++)
+		if (!nospcrlfcl(msg.at(j)) && msg.at(j) != ':')
+			return false;
+	*i += j;
+	return true;
+}
+
 bool	has_good_params(std::string msg, int *i)
 {
+	if (msg.at(i) == "\r" && msg.at(i + 1) != "\n")
+		return true;
+
+	int nb_space_middle = 0;
+	for (; 1; nb_space_middle++)
+		if (!is_space_middle(msg.substr(*i), i))
+			break;
+	
+	if (nb_space_middle > 14)
+		return false;
+	if (msg.at(*i) != ' ')
+		return false;
+	*i++;
+	if (nb_space_middle < 14 && msg.at(*i) != ':')
+		return false;
+	if (msg.at(*i) == ':')
+		*i++;
+	for (; 1; *i++)
+		if (msg.at(*i) != ':' && msg.at(*i) != ' ' && !nospcrlfcl(msg.at(*i)))
+			return false;
+
 	return true;
 }
 
