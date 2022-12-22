5bca12a303cc49c691b7ea672db64df407f34900
Romane LESEUR <rleseur@e2r8p20.clusters.42paris.fr>
2022-12-22 03:51:51 +0100

    command check - lil correction (not good yet)

--- srcs/commands/check.cpp
+++ srcs/commands/check.cpp
@@ -1,7 +1,11 @@
 #include <cctype>
+#include <string>
+#include <iostream>
 
-bool	has_prefix(std::string msg, size_t *i)
+bool	has_prefix(std::string msg, int *i)
 {
+	(void)msg;
+	(void)i;
 	return true;
 }
 
@@ -19,7 +23,7 @@ bool	is_good_command(std::string msg, int *i)
 	}
 
 	int j = 0;
-	for (; msg[j] != ' ' || msg[j] != '\r'; j++)
+	for (; msg[j] != ' ' && msg[j] != '\r'; j++)
 		if (!isalpha(msg[j]))
 			return false;
 	*i += j;
@@ -29,21 +33,32 @@ bool	is_good_command(std::string msg, int *i)
 
 bool	nospcrlfcl(char c)
 {
-	if (c == NULL || c == '\r' || c == '\n' || c == ' ' || c == ':')
+	if (c == 0 || c == '\r' || c == '\n' || c == ' ' || c == ':')
 		return false;
+	return true;
+}
+
+bool	is_trailing(std::string msg, int *i)
+{
+	for (; msg[*i] && msg.at(*i) != '\r'; (*i)++)
+		if (msg.at(*i) != ':' && msg.at(*i) != ' ' && !nospcrlfcl(msg.at(*i)))
+			return false;
+
 	return true;
 }
 
 bool	is_space_middle(std::string msg, int *i)
 {
 	int j = 0;
+
 	if (msg.at(j) != ' ')
 		return false;
 	j++;
 	if (!nospcrlfcl(msg.at(j)))
 		return false;
 	j++;
-	for (; 1; j++)
+	int k = 0;
+	for (; msg[j] && (msg.at(j + 1) == ':' || is_trailing(msg.substr(j + 1), &k)); j++)
 		if (!nospcrlfcl(msg.at(j)) && msg.at(j) != ':')
 			return false;
 	*i += j;
@@ -52,26 +67,30 @@ bool	is_space_middle(std::string msg, int *i)
 
 bool	has_good_params(std::string msg, int *i)
 {
-	if (msg.at(i) == "\r" && msg.at(i + 1) != "\n")
+	int n = 0;
+	if (msg.at(n) == '\r' && msg.at(n + 1) != '\n')
 		return true;
 
 	int nb_space_middle = 0;
-	for (; 1; nb_space_middle++)
-		if (!is_space_middle(msg.substr(*i), i))
+	int j = 0;
+	for (; 1; nb_space_middle++, j++)
+		if (!is_space_middle(msg.substr(j), &j))
 			break;
-	
+	n += j;
+
 	if (nb_space_middle > 14)
 		return false;
-	if (msg.at(*i) != ' ')
+	std::cout << "PASS" << std::endl;
+	if (msg.at(n) != ' ')
 		return false;
-	*i++;
-	if (nb_space_middle < 14 && msg.at(*i) != ':')
+	n++;
+	if (nb_space_middle < 14 && msg.at(n) != ':')
 		return false;
-	if (msg.at(*i) == ':')
-		*i++;
-	for (; 1; *i++)
-		if (msg.at(*i) != ':' && msg.at(*i) != ' ' && !nospcrlfcl(msg.at(*i)))
-			return false;
+	if (msg.at(n) == ':')
+		n++;
+	if (!is_trailing(msg.substr(n), &n))
+		return false;
+	*i += n;
 
 	return true;
 }
@@ -86,18 +105,22 @@ bool	has_good_params(std::string msg, int *i)
  */
 bool	is_good_message(std::string msg)
 {
-	size_t i = 0;
+	int i = 0;
 	if (msg.at(i) == ':')
 	{
-		if (!has_prefix(msg.substr(1), &i) || msg.at(i) != " ")
+		if (!has_prefix(msg.substr(1), &i) || msg.at(i) != ' ')
 			return false;
 		i++;
 	}
 	if (!is_good_command(msg.substr(i), &i))
 		return false;
+	std::cout << "PASS command" << std::endl;
 	if (!has_good_params(msg.substr(i), &i))
 		return false;
-	if (msg.at(i) != "\r" || msg.at(i + 1) != "\n")
+	std::cout << "PASS params" << std::endl;
+	if (msg.at(i) != '\r')
+		return false;
+	if (msg.at(i + 1) != '\n')
 		return false;
 	return true;
 }
---
+++ srcs/commands/test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+bool	is_good_message(std::string msg);
+
+int main()
+{
+	std::cout << std::boolalpha;
+	std::cout << is_good_message("test") << std::endl; //false
+	std::cout << is_good_message("JOIN help : \r\n") << std::endl; //true
+}
