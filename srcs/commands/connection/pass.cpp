3f08c749773e01abdf636ed86e4cb8346b61b010
vkrajcov <vkrajcov@student.42.fr>
2022-12-27 17:00:49 +0100

    Commit to change branch

--- srcs/commands/connection/pass.cpp
+++ srcs/commands/connection/pass.cpp
@@ -3,15 +3,14 @@
 
 void	pass(Command &cmd)
 {
-	// if (params.size() < 1)
-	// 	return ERR_NEEDMOREPARAMS(cmd.getUser().getUsername(), "PASS");
-	// if (params[0] != server->getPassword) //compare hashes?
-	// 	return ERR_PASSWDMISMATCH(user->getUsername());
-	// if (user->getIsConnected())
-	// 	return ERR_ALREADYREGISTERED(user->getUsername());
-	// user->setIsConnected();
-	// return "";
-	(void)cmd;
+	if (cmd->getParams().size() < 1)
+		cmd->setOutput(ERR_NEEDMOREPARAMS(cmd.getUser().getUsername(), "PASS"));
+	if (pcmd->getParams()[0] != Command::_server->getPassword()) //compare hashes?
+		cmd->setOutput(ERR_PASSWDMISMATCH(user->getUsername()));
+	if (user->getIsConnected())
+		cmd->setOutput(ERR_ALREADYREGISTERED(user->getUsername()));
+	user->setIsConnected();
+	cmd->setOutput("");
 }
 
 //check order
