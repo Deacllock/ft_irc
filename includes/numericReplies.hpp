ab121ae2fedb878b07991343a1c3bbba1ec53edc
GAsNa <romane.leseur.pro@gmail.com>
2022-12-29 11:39:11 +0100

    add numeric replies 405 474 475 476

--- includes/numericReplies.hpp
+++ includes/numericReplies.hpp
@@ -10,6 +10,7 @@ std::string rpl_created( std::string date ); //003
 std::string rpl_myinfo( std::string servername, std::string version, std::string user_modes, std::string channel_modes ); //004
 
 std::string err_nosuchchannel( std::string channel ); //403
+std::string err_toomanychannels( std::string channel ); //405
 std::string	err_toomanytargets( std::string target, std::string error_code, std::string abort_mess ); //407
 std::string err_nonicknamegiven(); //431
 std::string err_erroneusnickname( std::string nick ); //432
@@ -19,6 +20,9 @@ std::string	err_needmoreparams( std::string client, std::string command ); //461
 std::string	err_alreadyregistered( std::string client ); //462
 std::string err_channelisfull( std::string channel ); // 471
 std::string err_inviteonlychan( std::string channel ); //473
+std::string err_bannedfromchan( std::string channel ); //474
+std::string err_badchannelkey( std::string channel ); //475
+std::string err_bedchanmask( std::string channel ); //476
 std::string err_restricted(); //484 
 
 #endif
--- srcs/commands/numericReplies.cpp
+++ srcs/commands/numericReplies.cpp
@@ -7,7 +7,9 @@ std::string	rpl_yourhost( std::string servername, std::string version ) { return
 std::string rpl_created( std::string date ) { return "This server was created " + date; }
 std::string rpl_myinfo( std::string servername, std::string version, std::string user_modes, std::string channel_modes ) { return servername + " " + version + " " + user_modes + " " + channel_modes; }
 
+
 std::string err_nosuchchannel( std::string channel ) { return channel + " :No such channel"; }
+std::string err_toomanychannels( std::string channel ) { return channel + " :You have joined too many channels"; }
 std::string	err_toomanytargets( std::string target, std::string error_code, std::string abort_mess ) { return target + " :" + error_code + " recipients. " + abort_mess; }
 std::string	err_needmoreparams( std::string client, std::string command ) { return client + " " + command + ": Not enough parameters"; }
 std::string	err_alreadyregistered( std::string client ) { return client + " :You may not reregister"; }
@@ -19,7 +21,11 @@ std::string err_nicknameinuse( std::string nick ) { return nick + " :Nickname is
 std::string err_unavailableresource( std::string name ) { return name + " :Nick/channel is temporarily unavailable"; }
 std::string err_channelisfull(std::string channel) { return channel + " :Cannot join channel (+l)"; }
 std::string err_inviteonlychan(std::string channel) { return channel + " :Cannot join channel (+i)"; } // what is +i
+std::string err_bannedformchan(std::string channel) { return channel + " :Cannot join channel (+b)"; }
+std::string err_badchannelkey( std::string channel ) { return channel + " :Cannot join channel (+k)"; }
+std::string err_bedchanmask( std::string channel ) { return channel + " :Bad Channel Mask"; }
 std::string err_restricted() { return ":Your connection is restricted!"; }
+
 
 std::string error( std::string data ) { return  "Error: " + data; }
 
