e5b959e1f63aa7d6899ac6df4fa42c4ee563ff15
GAsNa <romane.leseur.pro@gmail.com>
2022-12-29 11:13:50 +0100

    init

--- includes/commandHandlers.hpp
+++ includes/commandHandlers.hpp
@@ -2,8 +2,10 @@
 # define COMMANDHANDLERS
 
 # include "Command.hpp"
-# include "NumericReplies.hpp"
+# include "numericReplies.hpp"
 
 void	pass( Command &cmd );
+void	nick( Command &cmd );
+void	join(Command &cmd);
 
 #endif
---
+++ srcs/commands/channel/join.cpp
@@ -0,0 +1,7 @@
+#include "CommandHandlers.hpp"
+
+void	join(Command &cmd)
+{
+	std::vector<std::string> channels;
+	std::vector<std::string> keys;
+}
