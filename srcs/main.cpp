4055a7de2821ba3554a0b469a4b45a83c60ab7e6
GAsNa <romane.leseur.pro@gmail.com>
2023-01-06 15:08:26 +0100

    signal ctrl+c handl but need to delete server

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <csignal>
 
 #include "Server.hpp"
+
+void	ctrl_c(int signum)
+{
+	(void)signum;
+	std::cout << "In ctrl C" << std::endl;
+	// delete server; 
+}
 
 int main(int argc, char *argv[])
 {
@@ -9,5 +17,6 @@ int main(int argc, char *argv[])
 		std::cerr << "Usage: ./ft_irc port password" << std::endl;
 		return (2);
 	}
+	signal(SIGINT, ctrl_c);
 	Server(argv[1], argv[2]);
 }
