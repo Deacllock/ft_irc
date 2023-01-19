ac6fe055f1fe75b80ba3cdc05c0a4b590d4f7b24
vkrajcov <vkrajcov@student.42.fr>
2023-01-19 10:26:27 +0100

    Added invite msg to user who is invited

--- srcs/commands/channel/invite.cpp
+++ srcs/commands/channel/invite.cpp
@@ -13,15 +13,19 @@ void	invite(Command &cmd)
 	Channel	*chan = Command::server->getChannelByName(channel);
 	if (!chan->isJoinedUser(usr))
 		return usr->pushReply(":" + cmd.server->getName() + " " + err_notonchannel(usr->getNickname(), chan->getName()));
+	
 	if (!chan->isOperatorUser(usr))
 		return usr->pushReply(":" + cmd.server->getName() + " " + err_chanoprivsneeded(chan->getName()));
+
 	if (!Command::server->isExistingUserByName(nickname))
 		return usr->pushReply(":" + cmd.server->getName() + " " + err_nosuchnick(usr->getNickname(), nickname));
+
 	User	*userToInvite = Command::server->getUserByName(nickname);
 	if (chan->isJoinedUser(userToInvite))
 		return usr->pushReply(":" + cmd.server->getName() + " " + err_useronchannel(usr->getNickname(), nickname, channel));
 
 	chan->addInvited(userToInvite);
 
-	usr->pushReply(":" + cmd.server->getName() + " " +rpl_inviting(usr->getNickname(), channel, nickname));
+	usr->pushReply(":" + cmd.server->getName() + " " + rpl_inviting(usr->getNickname(), channel, nickname));
+	userToInvite->pushReply(":" + usr->getFullName() + " INVITE " + nickname + " " + channel);
 }
