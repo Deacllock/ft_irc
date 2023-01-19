384d658455bbb5f4f6582d67fc7fae1854bfa903
vkrajcov <vkrajcov@student.42.fr>
2023-01-19 12:13:11 +0100

    Brief added to part + msg to all users

--- srcs/commands/channel/part.cpp
+++ srcs/commands/channel/part.cpp
@@ -1,13 +1,24 @@
 #include "commandHandlers.hpp"
 #include "utils.hpp"
 
+/**
+ * @brief  The PART command causes the user sending the message to be removed from channel(s).
+ * 
+ * Parameters: <channel> *( "," <channel> ) [ <Part Message> ]
+ * 
+ * @param cmd Contains command, parameters, user and server infos.
+ */
 void	part(Command cmd)
 {
 	User	*usr = cmd.getUser();
 
-	if (cmd.getParams().size() < 1)
+	if (cmd.getParams().size() == 0)
 		return usr->pushReply(":" + cmd.server->getName() + " " + err_needmoreparams(usr->getNickname(), "PART"));
 	
+	std::string reason = "";
+	if (cmd.getParams().size() > 1)
+		reason += " " + cmd.getParams()[1];
+
 	std::vector<std::string> channels = splitByComma(cmd.getParams()[0]);
 
 	std::vector<std::string>::iterator it = channels.begin();
@@ -19,14 +30,17 @@ void	part(Command cmd)
 			usr->pushReply(":" + cmd.server->getName() + " " + err_nosuchchannel(usr->getNickname(), *it));
 			continue;
 		}
+
 		Channel	*chan = Command::server->getChannelByName(*it);
 		if (!chan->isJoinedUser(usr))
-		{
 			usr->pushReply(":" + cmd.server->getName() + " " + err_notonchannel(usr->getNickname(), chan->getName()));
-			continue;
+
+		else
+		{
+			sendAll(chan->getUsers(), NULL, ":" + usr->getFullName() + " PART " + chan->getName() + reason);
+			chan->removeOperator(usr);
+			chan->removeUser(usr);
+			usr->removeJoinedChan(chan);
 		}
-		chan->removeOperator(usr);
-		chan->removeUser(usr);
-		usr->removeJoinedChan(chan);
 	}
 }
