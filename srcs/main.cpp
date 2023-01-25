422c1b2143168bd6af2fbe8125ab13343423f00e
GAsNa <romane.leseur.pro@gmail.com>
2023-01-25 17:45:49 +0100

    exception try catch in main

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -13,5 +13,7 @@ int main(int argc, char *argv[])
 		return (2);
 	}
 	signal(SIGINT, ctrl_c);
-	Server(argv[1], argv[2]);
+	try {
+		Server(argv[1], argv[2]);
+	} catch (std::exception &e) { std::cout << e.what() << std::endl; }
 }
